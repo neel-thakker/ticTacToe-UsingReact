d64eb87f4878f0f49c46e445aed15ffbe6a12c14
Neel Thakker <neelthakker2001@gmail.com>
2020-08-31 17:01:13 +0530

    Updates

---
+++ precache-manifest.bd274b42b250630aa08a2b5099f24462.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "634dfadf850f4edb1cb38103f3ea73a8",
+    "url": "/ticTacToe-UsingReact/index.html"
+  },
+  {
+    "revision": "05608b6ea6483b92d16f",
+    "url": "/ticTacToe-UsingReact/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "493d1041f1114e17ecda",
+    "url": "/ticTacToe-UsingReact/static/js/2.b1924a8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ticTacToe-UsingReact/static/js/2.b1924a8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05608b6ea6483b92d16f",
+    "url": "/ticTacToe-UsingReact/static/js/main.821d80db.chunk.js"
+  },
+  {
+    "revision": "fef8c57e6250e7e69455",
+    "url": "/ticTacToe-UsingReact/static/js/runtime-main.638277f1.js"
+  }
+]);
