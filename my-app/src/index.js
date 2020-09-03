d9c63223f8fa7635762d9f1a20281f9c7cfbd03e
Neel Thakker <neelthakker2001@gmail.com>
2020-09-03 15:27:56 +0530

    Starting Single Player Mode

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Game from "./components/GameComponent";
-
 // ========================================
 
 ReactDOM.render(<Game />, document.getElementById("root"));
